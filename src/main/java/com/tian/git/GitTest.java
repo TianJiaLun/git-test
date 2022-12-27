d7c0ce63d9c4eeff7a399bbfd525b7d39baeb71a
TianJiaLun <121298630+TianJiaLun@users.noreply.github.com>
2022-12-27 15:52:58 +0800

    pull commit

--- src/main/java/com/tian/git/GitTest.java
+++ src/main/java/com/tian/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("master test");
         System.out.println("hot-fix test");
-        System.out.println("push test");
+        System.out.println("push test66666666");
+        System.out.println("pull test66666666");
     }
 }
